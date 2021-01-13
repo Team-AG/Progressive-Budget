694367036e1741506490bc0cf4f6885e2f1a8d43
ANDREW <66812117+Team-AG@users.noreply.github.com>
2021-01-13 18:28:48 -0500

    Add files via upload

---
+++ Develop/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    newUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+// routes
+app.use(require("./routes/api.js"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}`);
+});
